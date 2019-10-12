2f88dfb7415c7e2c223637dbaf7da80542708c07
mgeimer2 <mgeimer2@illinois.edu>
2019-10-12 15:37:34 -0500

    <feat>Created function that takes a dataPath as String, and returns a Pandas dataframe representing the data in the CSV.

---
+++ Test/Pandas+NumPy/CSVtoSortedDataFrame.py
@@ -0,0 +1,26 @@
+import csv
+import pandas
+
+#dataPath is the file which is being imported to be turned into dataframe in the format: exampleFile.csv
+def importCSV(dataPath):
+	datafile = open(dataPath, 'r')
+	datareader = csv.reader(datafile, delimiter=',')
+	pandas.set_option('display.max_columns', 8)
+	pandas.set_option('display.max_rows', 500)
+	data = []
+	for row in datareader:
+		data.append(row)
+	lastData = []
+	for i in range(1,len(data)):
+		lastData.append(data[i])
+	dataFrame = pandas.DataFrame(lastData, columns=data[0])
+	return dataFrame
+
+def sortDataFrame(dataFrame):
+	dataFrame = dataFrame.sort_values(by = ['score', 'comms_num', 'created'], ascending = [False, False, True])
+	return dataFrame
+
+def getPandasDataFrame(dataPath):
+	return sortDataFrame(importCSV(dataPath))
+
+print(getPandasDataFrame('politics_top500.csv'))
